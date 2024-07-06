b077c3f79d15e8f2fe1e680ef497bd3ec87539c6
Tanjila Khatun <tanjilabithi12@gmail.com>
2024-07-06 20:35:35 +0600

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".input",{
+    strings:["E-commerce Website", "Fashion Store", "Grocery Website", "Shopping Website"],
+    typeSpeed:70,
+    backSpeed:60,
+    loop:true
+
+});
