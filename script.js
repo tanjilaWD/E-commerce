1d6934c11d9887d15cca0ce676245fcc7cfbef1a
Tanjila Khatun <tanjilabithi12@gmail.com>
2024-07-07 19:15:16 +0600

    new

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
 menu.onclick = () =>{
-    menu.classList.toggle(' bx-x');
+    menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
